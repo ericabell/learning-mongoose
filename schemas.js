38ea45de5a5b2dff469690becca2b79fecc2ffbd
Eric Abell <ericabell@users.noreply.github.com>
2017-08-21 22:37:46 -0500

    Up through virtuals

--- schemas.js
+++ schemas.js
@@ -101,11 +101,51 @@ Animal.findByName('fido', (err, animals) => {
   console.log('=> *****************************');
   console.log('=> Animals matching fido');
   console.log(animals);
-})
+});
 
 // and we can use the query helper we defined
 Animal.find().byName('fido').exec( (err, animals) => {
   console.log('=> using chainable queries we defined');
   console.log('=> Animals with name fido');
   console.log(animals);
-})
+});
+
+// *********************************************************************
+// Indexes
+
+animalSchema.index({name: 1, type: -1}); // schema level index
+
+// *********************************************************************
+// Virtuals - doc props you can get and set but do not persist in db
+let personSchema = new Schema({
+  name: {
+    first: String,
+    last: String
+  }
+});
+
+personSchema.virtual('fullName').
+  get( function () { return this.name.first + ' ' + this.name.last; }).
+  set( function (v) {
+    this.name.first = v.substr(0, v.indexOf(' '));
+    this.name.last = v.substr(v.indexOf(' ') + 1);
+  });
+
+let Person = mongoose.model('Person', personSchema);
+
+let axl = new Person({
+  name: {first: 'Axl', last: 'Rose'}
+});
+
+// you could print their name this way...
+console.log('Print the normal way:');
+console.log(axl.name.first + ' ' + axl.name.last);
+
+// now we can call the getter function any time we need full name
+console.log('Print using a virtual:');
+console.log(axl.fullName);
+
+// and we can also set the first and last name from fullName
+axl.fullName = 'William Rose';
+console.log('Set using a virtual:');
+console.log(axl.name.first + ' ' + axl.name.last);
